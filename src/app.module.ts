77e2ac4a9c89f8d95880817dd2bc2dfac7ddd3b0
Matheus <halkner77@hotmail.com>
2023-01-14 21:27:06 -0300

    :sparkles: feat: Adding some providers and configure connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,28 @@
+import { AppController } from './app.controller';
+import { userProviders } from './users/users.providers';
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { ConfigModule } from '@nestjs/config';
+import { databaseProviders } from './core/database/database.providers';
+import { UsersModule } from './users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './core/auth/auth.service';
+import { JwtStrategy } from './core/auth/guards/strategy/jwt-strategy';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    autoLoadEntities: true,
-    synchronize: true,
-  })],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRES
+      }
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [...databaseProviders, ...userProviders, AuthService, JwtStrategy],
 })
 export class AppModule {}
