6e915629aa80307e8fed514f183edf87110136f0
Matheus <halkner77@hotmail.com>
2023-01-15 10:40:24 -0300

    :sparkles: feat: Change Password route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,9 @@
+import { JwtAuthGuard } from './core/auth/guards/jwt-auth.guard';
 import { Body, Post, Controller, Patch, UseGuards } from '@nestjs/common';
 import { AuthService } from "./core/auth/auth.service";
 import { ChangePasswordDto } from './users/dto/change-password.dto';
 import { CreateUserDto } from './users/dto/create-user.dto';
 import { LoginDto } from './users/dto/login.dto';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
@@ -22,7 +22,7 @@ export class AppController {
       return await this.authService.login(loginDto);
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   @Patch('/auth/change-password')
   async changePassword(@Body() changePasswordDto: ChangePasswordDto) {
     return this.authService.changePassword(changePasswordDto);
